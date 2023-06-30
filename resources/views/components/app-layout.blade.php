66f524a263fe726c7eb1d48c392b6dfa03661fb8
M Fachri <mfachri@upi.edu>
2023-06-30 19:07:42 +0700

    second commit

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -7,6 +7,7 @@
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}" />
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="/css/app.css">
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -16,7 +17,7 @@
     </title>
 
     <!-- CSS & JS Assets -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -81,7 +82,7 @@
     @isset($script)
         {{ $script }}
     @endisset
-
+    <script src="/js/app.js"></script>
 </body>
 
 </html>
